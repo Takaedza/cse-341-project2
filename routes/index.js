133fb351f4b79961c21f45412a4f8404a4afef3d
Takaedza <tmandie@live.com>
2024-10-27 05:39:28 +0200

    auth

--- routes/index.js
+++ routes/index.js
@@ -14,11 +14,11 @@ router.use('/', require('./swagger'));
 router.use('/customers', require('./customers'));
 router.use('/product', require('./product'));
 
-router.get('/logout', (req, res, next) => {
+/*router.get('/logout', (req, res, next) => {
   req.logout(function(err) {
     if (err) {return next(err); }
     res.redirect('/')
   });
-})
+})*/
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-const passport = require('passport');
-const session = require('express-session');
-const GitHubStrategy = require('passport-github2').Strategy;
+//const passport = require('passport');
+//const session = require('express-session');
+//const GitHubStrategy = require('passport-github2').Strategy;
 const cors = require('cors');
 
 const port = process. env.Port || 3001;
@@ -12,7 +12,16 @@ const app = express();
 
 app
     .use(bodyParser.json())
-    .use(session({
+    .use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+        );
+        res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
+    /*.use(session({
         secret: "secret",
         resave: false,
         saveUninitialized: true,
@@ -33,7 +42,7 @@ app
             'POST, GET, PUT, PATCH, OPTIONS, DELETE'
         );
         next();
-    })
+    })*/
     .use(cors({methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']}))
     .use(cors({origin: '*'}))
     .use('/', require('./routes'));
@@ -49,7 +58,7 @@ app
 
 //app.use('/', require('./routes'));
 
-    passport.use(new GitHubStrategy({
+   /* passport.use(new GitHubStrategy({
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL: process.env.CALLBACK_URL
@@ -76,7 +85,7 @@ app.get('/github/callback', passport.authenticate('github', {
         req.session.user = req.user;
         res.redirect('/');
     }
-)
+) */
 
 process.on('uncaughtException', (err, origin) =>{
     console.log(process.stderr.fd, 'Caught exception: ' + err + 'Exception Origin: ' + origin);
