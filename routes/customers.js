d25d67605e4bce3fcec5efbe1e296d6ea680eb4c
Takaedza <tmandie@live.com>
2024-10-27 04:57:48 +0200

    valid inserted

--- routes/customers.js
+++ routes/customers.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 
 const customersController = require('../controllers/customers');
-//const validation = require('../middleware/validate');
+const validation = require('../middleware/validate');
 const {isAuthenticated} = require('../middleware/authenticate');
 
 router.get('/', customersController.getAll);
 
 router.get('/:id', customersController.getSingle);
 
-router.post('/', /*validation.saveCustomer,*/ isAuthenticated, customersController.createCustomer);
+router.post('/', validation.saveCustomer, isAuthenticated, customersController.createCustomer);
 
-router.put('/:id', /*validation.saveCustomer,*/ isAuthenticated, customersController.updateCustomer);
+router.put('/:id', validation.saveCustomer, isAuthenticated, customersController.updateCustomer);
 
 router.delete('/:id', isAuthenticated, customersController.deleteCustomer);
 
